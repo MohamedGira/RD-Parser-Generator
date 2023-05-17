876c400b54230bdf8804f1352621a71928e026e2
MohamedGira <mohamedgira0901@gmail.com>
2023-05-17 06:56:34 +0300

    added more token types

--- Tokens/TokenTypes.py
+++ Tokens/TokenTypes.py
@@ -32,7 +32,7 @@ class Token_type(Enum): # listing all tokens type
     Implicit=26
     none=27
     Prints=28
-    Reads=29
+    Read=29
     Comment=30
     Scopeop=31
     Comma=32
@@ -43,5 +43,63 @@ class Token_type(Enum): # listing all tokens type
     OpenBrac=37
     CloseBrac=38
     Literal=39
+    newLine=40
+    Semicolon=41
+    Write=42
 
-print(Token_type['Begin'])
+
+
+
+ReservedWords={
+            "begin":Token_type.Begin,
+            "end":Token_type.End,
+            "do":Token_type.Do,
+            "if":Token_type.If,
+            "then":Token_type.Then,
+            "else":Token_type.Else,
+            "integer":Token_type.Integer,
+            "real":Token_type.Real,
+            "complex":Token_type.Complex,
+            "parameter":Token_type.Parameter,
+            "character":Token_type.Character,
+            "program":Token_type.Program,
+            "implicit":Token_type.Implicit,
+            "none":Token_type.none,
+            "print":Token_type.Prints,
+            "read":Token_type.Read,
+            "write":Token_type.Read,
+            "!":Token_type.Comment,
+            
+            }
+Operators={
+            ",":Token_type.Comma,
+            "::":Token_type.Scopeop,
+            "<":Token_type.Lessthanop,
+            ">":Token_type.Greaterthanop,
+            "<=":Token_type.Lessthanorequalop,
+            ">=":Token_type.Greaterthanorequalop,
+            "/=":Token_type.Notequalop,
+            "==":Token_type.Isequalop,
+            "=":Token_type.Equalop,
+            "+":Token_type.Plusop,
+            "-":Token_type.Minusop,
+            "*":Token_type.Multiplyop,
+            "/":Token_type.Divideop,
+            "(":Token_type.OpenParan,
+            ")":Token_type.CloseParan,
+            "[":Token_type.OpenBrac,
+            "]":Token_type.CloseBrac,
+            ".":Token_type.Dot,
+            ";":Token_type.Semicolon
+          }
+
+
+def get_lower_dict(enum):
+    d={}
+    enum_keys = list(enum.__members__.keys())
+    for key in enum_keys:
+        d[key.lower()]=enum[key]
+    return d
+
+
+
