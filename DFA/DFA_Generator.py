e6307cda8b0bfc44894ecbd62156154f34501216
MohamedGira <mohamedgira0901@gmail.com>
2023-05-25 11:29:15 +0300

    moved DFA modules to separate package

--- DFA/DFA_Generator.py
+++ DFA/DFA_Generator.py
@@ -1,23 +1,8 @@
-from grapher import *
+from DFA.grapher import *
 from Tokens.TokenTypes import *
 
 
-def regictify_dict(dic):
-    """
-    takes dfa dict representation and completes it by adding reject state
-    """
-    d={}
-    
-    reject_node=Node('reject')
-    for i in dic.keys():
-        s=[]
-        for j in dic[i].keys():
-          s.append(dic[i][j])
-        a='|'.join(s)
-        dic[i].update({reject_node:f'^(?!{a})'})
-    dic.update({reject_node:{reject_node:'(.)+'}})
-       
-    return dic
+
 
 words=[
 ['Begin'],
@@ -72,44 +57,46 @@ for wordarr in words:
 
 #identifier DFA
 i_arr=[Node('1'),Node('2',state=1)]
-identifier_dict=regictify_dict({
+identifier_dict=DFA.regictify_dict({
     i_arr[0]:{i_arr[1]:'[_A-Za-z]'},
     i_arr[1]:{i_arr[1]:'[_a-zA-Z0-9]'},    
 })
-#test
-#Visualizer.GIF_NO_SPLIT(i_arr[0],identifier_dict,'2aa22_s',True)
 
-
-#Const DFA
+#Const real DFA
 i=[Node('q0'),Node('q1'),Node('q2',state=1),Node('q3',state=1)]
-constr_dict=regictify_dict( {
+constr_dict=DFA.regictify_dict( {
     i[0]:{i[1]:'\+|-',i[2]:'[0-9]'},#start state
     i[1]:{i[2]:'[0-9]',i[3]:'\.'},#sign state
     i[2]:{i[2]:'[0-9]',i[3]:'\.',},#int state
     i[3]:{i[3]:'[0-9]'},#double state
 })
-
+#Const int DFA
 i=[Node('q0'),Node('q1'),Node('q2',state=1)]
-consti_dict=regictify_dict( {
+consti_dict=DFA.regictify_dict( {
     i[0]:{i[1]:'\+|-',i[2]:'[0-9]'},#start state
     i[1]:{i[2]:'[0-9]'},#sign state
     i[2]:{i[2]:'[0-9]'},#int state
 })
-#test
-#Visualizer.GIF_NO_SPLIT(i[0],const_dict,'2aa22_s',True)
 
 #Literal DFA
-i=[Node('q0'),Node('q1'),Node('q2'),Node('q3',state=1)]
-
-literal_dict=regictify_dict( {
-    i[0]:{i[1]:'\'',i[2]:'"'},#start state
+i=[Node('q0'),Node('q1'),Node('q2'),Node('q3',state=1),Node('reject')]
+literal_dict= {
+    i[0]:{i[1]:'\'',i[2]:'"',i[4]:'^(?!\'|")'},#start state
     i[1]:{i[1]:'^(?!\')',i[3]:'\''},#singe qoutation state
     i[2]:{i[2]:'^(?!")',i[3]:'"'},#double qoutation state
-    i[3]:{i[3]:''},#final state
-})
+    i[3]:{i[3]:'',i[4]:'(.)+'},#final state
+    i[4]:{i[4]:'(.)+'},#reject state
+}
 
+#Comment DFA
+i=[Node('q0'),Node('q1',state=1),Node('reject')]
+comment_dict= {
+    i[0]:{i[1]:'!',i[2]:'^(?!!)'},#nothing state
+    i[1]:{i[2]:'\n',i[1]:'^(?!\n)'},#comment state   
+    i[2]:{i[2]:'(.)+'},#final state
+}
 #test
-#Visualizer.GIF_NO_SPLIT(i[0],literal_dict,"'2aa22_s'",True)
+
 def inDFA_dict(dfadic,strr,strrdict):
     dfadic[Token_type[strr]]=DFA('')
     dfadic[Token_type[strr]].dict=strrdict
@@ -118,6 +105,6 @@ inDFA_dict(DFA_dict,'Literal',literal_dict)
 inDFA_dict(DFA_dict,'ConstantI',consti_dict)
 inDFA_dict(DFA_dict,'ConstantR',constr_dict)
 inDFA_dict(DFA_dict,'Identifier',identifier_dict)
-#print(DFA_dict)
-#DFA_dict[Token_type.ConstantI].try_word('43242342.3')
+inDFA_dict(DFA_dict,'Comment',comment_dict)
+
 
